5a08edfa24e374f86e547fa83c712983aa3f47c7
Shamir Duran <shamirduran15@gmail.com>
2020-07-16 09:04:04 -0500

    dfin prueba

--- app/src/main/java/com/example/appseries/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/appseries/ui/activities/MainActivity.kt
@@ -3,60 +3,31 @@ package com.example.appseries.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.appseries.R
 import com.example.appseries.ui.fragments.FavoritesFragment
 import com.example.appseries.ui.fragments.HomeFragment
 import com.example.appseries.ui.fragments.PerfilFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var homeFragment: HomeFragment
-    private lateinit var favoritesFragment: FavoritesFragment
-    private lateinit var perfilFragment: PerfilFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        configNav()
 
-        homeFragment = HomeFragment()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.mainFragment, homeFragment)
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-            .commit()
 
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.nav_menu)
+    }
 
-        bottomNavigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    homeFragment = HomeFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.mainFragment, homeFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .commit()
-                }
-                R.id.favorites -> {
-                    favoritesFragment = FavoritesFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.mainFragment, favoritesFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .commit()
-                }
-                R.id.perfil -> {
-                    perfilFragment = PerfilFragment()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.mainFragment, perfilFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .commit()
-                }
-            }
-            true
-        }
+    private fun configNav() {
+        NavigationUI.setupWithNavController(
+            nav_menu,
+            Navigation.findNavController(this, R.id.mainFragment)
+        )
     }
 }
