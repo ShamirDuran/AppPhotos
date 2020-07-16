df375a3e937c075cd4d24fc34a9ffb42199f377e
Shamir Duran <shamirduran15@gmail.com>
2020-07-16 08:49:12 -0500

    Se termino la botton navbar

--- app/src/main/java/com/example/appseries/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/appseries/ui/activities/MainActivity.kt
@@ -3,14 +3,11 @@ package com.example.appseries.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
-import androidx.fragment.app.transaction
-import androidx.navigation.findNavController
 import com.example.appseries.R
 import com.example.appseries.ui.fragments.FavoritesFragment
 import com.example.appseries.ui.fragments.HomeFragment
 import com.example.appseries.ui.fragments.PerfilFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,39 +19,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigation:ChipNavigationBar = findViewById(R.id.nav_menu)
 
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainFragment, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+            .commit()
 
-        bottomNavigation.setOnItemSelectedListener { id ->
-            when (id) {
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.nav_menu)
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
                 R.id.home -> {
                     homeFragment = HomeFragment()
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.mainFragment, homeFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                         .commit()
                 }
                 R.id.favorites -> {
-                    homeFragment = HomeFragment()
+                    favoritesFragment = FavoritesFragment()
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.mainFragment, favoritesFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                         .commit()
                 }
                 R.id.perfil -> {
-                    homeFragment = HomeFragment()
+                    perfilFragment = PerfilFragment()
                     supportFragmentManager
                         .beginTransaction()
                         .replace(R.id.mainFragment, perfilFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                         .commit()
                 }
             }
-
             true
         }
-
     }
 }
