5b1a21792127db4b57a75b96c427befcfdead0c8
Shamir Duran <shamirduran15@gmail.com>
2020-07-29 20:41:16 -0500

    Sign out listo, procedo a tirarme la db

--- app/src/main/java/com/example/appseries/ui/fragments/PerfilFragment.kt
+++ app/src/main/java/com/example/appseries/ui/fragments/PerfilFragment.kt
@@ -1,11 +1,15 @@
 package com.example.appseries.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.appseries.R
+import com.example.appseries.ui.activities.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_perfil.*
 
 /**
  * A simple [Fragment] subclass.
@@ -13,11 +17,25 @@ import com.example.appseries.R
  * create an instance of this fragment.
  */
 class PerfilFragment : Fragment() {
+
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btSingOut.setOnClickListener {
+            auth.signOut()
+
+            startActivity(Intent(context, LoginActivity::class.java))
+            activity?.finish()
+        }
     }
 }
